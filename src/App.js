eb603b63386335c9a6cac87bb0a66c2deeaeba48
natalieermeryd <natalie.ermeryd@outlook.com>
2022-12-16 12:16:29 +0100

    continues

--- src/App.js
+++ src/App.js
@@ -3,9 +3,34 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Natalie CV</h1>
-      </header>
+      <div className="appSidebar">
+        <div className="appSideContainer">
+          <div id="sidePic" className="sidebarBoxes">
+            PIC
+          </div>
+          <div className="sidebarBoxes">1</div>
+          <div className="sidebarBoxes">2</div>
+          <div className="sidebarBoxes">3</div>
+        </div>
+      </div>
+      <div className="appContainer">
+        <div className="appHeader">
+          <h1>Natalie Ermeryd Stamer</h1>
+          <h2>Frontend Developer</h2>
+        </div>
+        <div id="appFirst" className="appBoxes">
+          1
+        </div>
+        <div id="appSecond" className="appBoxes">
+          2
+        </div>
+        <div id="appThird" className="appBoxes">
+          3
+        </div>
+        <div id="appFourth" className="appBoxes">
+          4
+        </div>
+      </div>
     </div>
   );
 }
